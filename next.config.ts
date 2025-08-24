d00b5286040ac0796648c8b1abc66afc19ae36fd
Kanwal19 <hkps17@gmail.com>
2025-08-24 17:38:33 +0530

    Update basePath

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  basePath: '',
+  basePath: '/portfolio-website',
   images: {
     unoptimized: true,
   },
